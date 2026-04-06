c8641e5648c2d860bf3371defad6e5fd6c9390b7
Abdulahi HIrsi <141289773+Lali9012@users.noreply.github.com>
2026-04-06 14:56:15 -0700

    Refactor hw1p1 module and update signal declarations

--- hw1p1.sv
+++ hw1p1.sv
@@ -1,25 +1,29 @@
+/*hw1p1: A sequential circuit with a D flip-flop and a full adder
+  Inputs: reset, clk, x, and y
+  Output: S (sum output)* Q flows back into the adder after storing the carry (state).
+
+*/
+
 module hw1p1 (x, y, clk, reset, S); 
-    input logic x,
-    input logic y,
-    input logic  clk,
-    input logic  reset,
-    output logic s,
 
-);
+    input x, y, clk, reset;
+    output S;
 
-    logic Q;
-    logic c;
+    reg Q;
+    wire C;
+    wire S;
+
 
     assign S = x ^ y ^ Q;
     assign C = ( x&y) | ( x& Q) | ( y & Q);
     
-    always_ff @( posedge clk or posedge reset)  begin
+    always @( posedge clk or posedge reset)  begin
 
-    if (reset)
-      Q <= 0;
+      if (reset)
+       Q <= 1'b0;
 
-    else
-      Q <= C;
+     else
+       Q <= C;
    end
 
 endmodule
